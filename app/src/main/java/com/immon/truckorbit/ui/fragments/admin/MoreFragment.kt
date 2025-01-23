2859b3c985ef6d078a9d1198ce1b93a13389a192
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-01-24 01:12:57 +0600

    Add settings page layout

--- app/src/main/java/com/immon/truckorbit/ui/fragments/admin/MoreFragment.kt
+++ app/src/main/java/com/immon/truckorbit/ui/fragments/admin/MoreFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.immon.truckorbit.data.LocalDB
 import com.immon.truckorbit.databinding.FragmentMoreBinding
+import com.immon.truckorbit.ui.activities.MainActivity
+import com.immon.truckorbit.ui.fragments.LandingFragment
 import com.immon.truckorbit.utils.applyWindowInsets
 import com.immon.truckorbit.utils.setLightStatusBar
 
@@ -26,6 +29,12 @@ class MoreFragment : Fragment() {
         binding = FragmentMoreBinding.inflate(inflater, container, false)
 
         binding.profileLayout.applyWindowInsets(top = true, bottom = false)
+
+        binding.logoutLayout.setOnClickListener {
+            LocalDB.putBoolean("logged_in", false)
+
+            MainActivity.replaceFragment(LandingFragment())
+        }
 
         return binding.root
     }
