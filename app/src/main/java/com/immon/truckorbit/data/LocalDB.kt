b1109e8a34e0318b4f15d6f404a25c48b157b568
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-01-28 00:25:13 +0600

    Convert java to kotlin

--- app/src/main/java/com/immon/truckorbit/data/LocalDB.kt
+++ app/src/main/java/com/immon/truckorbit/data/LocalDB.kt
@@ -1,92 +1,90 @@
-package com.immon.truckorbit.data;
+package com.immon.truckorbit.data
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Context
+import android.content.SharedPreferences
+import com.immon.truckorbit.BuildConfig
+import com.immon.truckorbit.TruckOrbit
 
-import com.immon.truckorbit.BuildConfig;
-import com.immon.truckorbit.TruckOrbit;
+@Suppress("unused")
+object LocalDB {
+    private var prefs: SharedPreferences = TruckOrbit
+        .getAppContext()
+        .createDeviceProtectedStorageContext()
+        .getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE)
 
-@SuppressWarnings("unused")
-public class LocalDB {
+    private var editor: SharedPreferences.Editor = prefs.edit()
 
-    public static SharedPreferences prefs = TruckOrbit
-            .getAppContext()
-            .createDeviceProtectedStorageContext()
-            .getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
-
-    static SharedPreferences.Editor editor = prefs.edit();
-
-    public static void putBoolean(String key, boolean val) {
-        editor.putBoolean(key, val).apply();
+    fun putBoolean(key: String?, `val`: Boolean) {
+        editor.putBoolean(key, `val`).apply()
     }
 
-    public static void putInt(String key, int val) {
-        editor.putInt(key, val).apply();
+    fun putInt(key: String?, `val`: Int) {
+        editor.putInt(key, `val`).apply()
     }
 
-    public static void putLong(String key, long val) {
-        editor.putLong(key, val).apply();
+    fun putLong(key: String?, `val`: Long) {
+        editor.putLong(key, `val`).apply()
     }
 
-    public static void putFloat(String key, float val) {
-        editor.putFloat(key, val).apply();
+    fun putFloat(key: String?, `val`: Float) {
+        editor.putFloat(key, `val`).apply()
     }
 
-    public static void putString(String key, String val) {
-        editor.putString(key, val).apply();
+    fun putString(key: String?, `val`: String?) {
+        editor.putString(key, `val`).apply()
     }
 
-    public static boolean getBoolean(String key) {
-        return prefs.getBoolean(key, false);
+    fun getBoolean(key: String?): Boolean {
+        return prefs.getBoolean(key, false)
     }
 
-    public static boolean getBoolean(String key, Boolean defValue) {
-        return prefs.getBoolean(key, defValue);
+    fun getBoolean(key: String?, defValue: Boolean): Boolean {
+        return prefs.getBoolean(key, defValue)
     }
 
-    public static int getInt(String key) {
-        return prefs.getInt(key, 0);
+    fun getInt(key: String?): Int {
+        return prefs.getInt(key, 0)
     }
 
-    public static int getInt(String key, int defValue) {
-        return prefs.getInt(key, defValue);
+    fun getInt(key: String?, defValue: Int): Int {
+        return prefs.getInt(key, defValue)
     }
 
-    public static long getLong(String key) {
-        return prefs.getLong(key, 0);
+    fun getLong(key: String?): Long {
+        return prefs.getLong(key, 0)
     }
 
-    public static long getLong(String key, long defValue) {
-        return prefs.getLong(key, defValue);
+    fun getLong(key: String?, defValue: Long): Long {
+        return prefs.getLong(key, defValue)
     }
 
-    public static float getFloat(String key) {
-        return prefs.getFloat(key, 0);
+    fun getFloat(key: String?): Float {
+        return prefs.getFloat(key, 0f)
     }
 
-    public static float getFloat(String key, float defValue) {
-        return prefs.getFloat(key, defValue);
+    fun getFloat(key: String?, defValue: Float): Float {
+        return prefs.getFloat(key, defValue)
     }
 
-    public static String getString(String key) {
-        return prefs.getString(key, null);
+    fun getString(key: String?): String? {
+        return prefs.getString(key, null)
     }
 
-    public static String getString(String key, String defValue) {
-        return prefs.getString(key, defValue);
+    fun getString(key: String?, defValue: String?): String? {
+        return prefs.getString(key, defValue)
     }
 
-    public static void clearPref(String key) {
-        editor.remove(key).apply();
+    fun clearPref(key: String?) {
+        editor.remove(key).apply()
     }
 
-    public static void clearPrefs(String... keys) {
-        for (String key : keys) {
-            editor.remove(key).apply();
+    fun clearPrefs(vararg keys: String?) {
+        for (key in keys) {
+            editor.remove(key).apply()
         }
     }
 
-    public static void clearAllPrefs() {
-        editor.clear().apply();
+    fun clearAllPrefs() {
+        editor.clear().apply()
     }
 }
