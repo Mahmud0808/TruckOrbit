99e865db2f493c9b1c04a21b8640a7cbb3bad328
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-01-31 01:48:28 +0600

    Add more settings items functionality

--- app/src/main/java/com/immon/truckorbit/ui/fragments/admin/MoreFragment.kt
+++ app/src/main/java/com/immon/truckorbit/ui/fragments/admin/MoreFragment.kt
@@ -1,15 +1,19 @@
 package com.immon.truckorbit.ui.fragments.admin
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.immon.truckorbit.data.LocalDB
 import com.immon.truckorbit.databinding.FragmentMoreBinding
 import com.immon.truckorbit.ui.activities.MainActivity
 import com.immon.truckorbit.ui.fragments.LandingFragment
 import com.immon.truckorbit.ui.fragments.base.BaseFragment
 import com.immon.truckorbit.utils.applyWindowInsets
+
 
 class MoreFragment : BaseFragment() {
 
@@ -36,10 +40,32 @@ class MoreFragment : BaseFragment() {
         binding.showIdleTrucksSwitch.setOnCheckedChangeListener { _, isChecked ->
             LocalDB.putBoolean("show_idle_trucks", isChecked)
         }
+        binding.showIdleTrucksLayout.setOnClickListener {
+            binding.showIdleTrucksSwitch.performClick()
+        }
 
         binding.showAdminsSwitch.isChecked = LocalDB.getBoolean("show_admins", false)
         binding.showAdminsSwitch.setOnCheckedChangeListener { _, isChecked ->
             LocalDB.putBoolean("show_admins", isChecked)
+        }
+        binding.showAdminsLayout.setOnClickListener {
+            binding.showAdminsSwitch.performClick()
+        }
+
+        binding.privacyLayout.setOnClickListener {
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.privacypolicies.com/generic/")))
+            } catch (e: Exception) {
+                Toast.makeText(requireContext(), "Unable to open link", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.helpLayout.setOnClickListener {
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.facebook.com/immon.abeer.5#")))
+            } catch (e: Exception) {
+                Toast.makeText(requireContext(), "Unable to open link", Toast.LENGTH_SHORT).show()
+            }
         }
 
         return binding.root
